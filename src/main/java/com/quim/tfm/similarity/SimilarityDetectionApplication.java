799a41c518ee5777617c40e7d879a4bbeb164dda
Quim <joaquim.motger@est.fib.upc.edu>
2019-11-05 12:24:33 +0100

    add intellij project

---
+++ src/main/java/com/quim/tfm/similarity/SimilarityDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.quim.tfm.similarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimilarityDetectionApplication.class, args);
+    }
+
+}
